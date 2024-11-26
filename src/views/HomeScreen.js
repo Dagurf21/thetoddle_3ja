5a6c8985d983197a1b45927b8117cdaa17744086
tinnamariat <tinnamariat@gmail.com>
2024-11-26 18:26:12 +0000

    Homescreen
    Style homescreen

--- src/views/HomeScreen.js
+++ src/views/HomeScreen.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
+import styles from '../styles/GlobalStyles.js';
 import {
     View,
     Text,
-    StyleSheet,
     FlatList,
     Button,
     Image,
@@ -10,14 +10,38 @@ import {
     Modal,
     TouchableOpacity,
     ScrollView,
+    Dimensions
 } from 'react-native';
 import { useDataContext } from '../services/DataContext';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
 
 const HomeScreen = ({ navigation }) => {
     const { boards, createBoard } = useDataContext();
     const [isModalVisible, setIsModalVisible] = useState(false);
+    const [isOptionsVisible, setIsOptionsVisible] = useState(false);
     const [newBoardName, setNewBoardName] = useState('');
     const [newBoardThumbnail, setNewBoardThumbnail] = useState('');
+    const [menuPosition, setMenuPosition] = useState({ x: 0, y: 0 });
+    const [menuDimensions, setMenuDimensions] = useState({ width: 0, height: 0 });
+    const screenWidth = Dimensions.get('window').width;
+    const screenHeight = Dimensions.get('window').height;
+
+    const handlePressOptions = (event) => {
+        const { pageX, pageY } = event.nativeEvent;
+        setMenuPosition({ x: pageX -10, y: pageY -20 });
+        setIsOptionsVisible(true);
+    };
+
+    const adjustedMenuPosition = {
+        top: menuPosition.y + menuDimensions.height > screenHeight
+            ? screenHeight - menuDimensions.height - 10
+            : menuPosition.y,
+        left: menuPosition.x + menuDimensions.width > screenWidth
+            ? screenWidth - menuDimensions.width - 10
+            : menuPosition.x,
+    };
+
 
     const handleAddBoard = () => {
         if (!newBoardName.trim() || !newBoardThumbnail.trim()) {
@@ -26,7 +50,7 @@ const HomeScreen = ({ navigation }) => {
         }
 
         const newBoard = {
-            id: boards.length + 1, // Generate a new ID
+            id: boards.length + 1,
             name: newBoardName,
             thumbnailPhoto: newBoardThumbnail,
         };
@@ -34,65 +58,156 @@ const HomeScreen = ({ navigation }) => {
         createBoard(newBoard);
         setNewBoardName('');
         setNewBoardThumbnail('');
-        setIsModalVisible(false); // Close the modal
+        setIsModalVisible(false);
+
     };
 
     return (
         <ScrollView contentContainerStyle={styles.scrollContainer}>
-            <Text style={styles.header}>Boards</Text>
+            <Text style={styles.header}>Your boards</Text>
+
+        {/*Boards*/}
             <FlatList
                 data={boards}
                 keyExtractor={(item) => item.id.toString()}
                 renderItem={({ item }) => (
-                    <View style={styles.boardItem}>
+
+                    <TouchableOpacity
+                        style={styles.boardItem}
+                        onPress={() => navigation.navigate('BoardDetail', { boardId: item.id })}
+                    >
                         <Image
                             source={{ uri: item.thumbnailPhoto }}
                             style={styles.thumbnail}
                         />
-                        <View style={styles.boardDetails}>
-                            <Text style={styles.boardName}>{item.name}</Text>
-                            <Button
-                                title="View Board"
-                                onPress={() =>
-                                    navigation.navigate('BoardDetail', { boardId: item.id })
-                                }
+                {/*Board name*/}
+                        <Text style={styles.boardButtonText}>{item.name}</Text>
+
+                {/*More options*/}
+                        <TouchableOpacity
+                            style={styles.optionsContainer}
+                            onPress={(event) => handlePressOptions(event)}>
+                            <MaterialIcons
+                                style={styles.threeDots}
+                                name="more-vert"
+                                size={32}
                             />
-                        </View>
-                    </View>
+                        </TouchableOpacity>
+                    </TouchableOpacity>
                 )}
-                // Ensure FlatList scrolls independently
+
                 contentContainerStyle={styles.flatListContainer}
-            />
-            <Button
-                title="Add New Board"
-                onPress={() => setIsModalVisible(true)}
+
             />
 
-            {/* Modal for adding a new board */}
+        {/*Modal for more options*/}
+            <Modal
+                visible={isOptionsVisible}
+                transparent={true}
+                animationType='none'
+                onRequestClose={() => setIsOptionsVisible(false)}
+            >
+                <TouchableOpacity
+                    style={{ flex: 1 }}>
+                    {isOptionsVisible && (
+                        <View
+                            style={[
+                                styles.optionsMenu,
+                                {
+                                    top: adjustedMenuPosition.top,
+                                    left: adjustedMenuPosition.left,
+                                },
+                            ]}
+                            onLayout={(event) => {
+                                const { width, height } = event.nativeEvent.layout;
+                                setMenuDimensions({ width, height });
+                            }}
+                        >
+                            <TouchableOpacity onPress={() => console.log('Edit')} style={styles.menuOption}>
+                                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                    <MaterialIcons
+                                        name="edit"
+                                        size={24}
+                                        color="#9E9E9E"
+                                        style={{ marginRight: 8 }}
+                                    />
+                                    <Text style={styles.menuItemText}>Edit</Text>
+                                </View>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity onPress={() => console.log('Delete')} style={styles.menuOption}>
+                                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                    <MaterialIcons
+                                        name="delete"
+                                        size={24}
+                                        color="#9E9E9E"
+                                        style={{ marginRight: 8 }}
+                                    />
+                                    <Text style={styles.menuItemText}>Delete</Text>
+                                </View>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity onPress={() => setIsOptionsVisible(false)} style={styles.menuOption}>
+                                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                    <MaterialIcons
+                                        name="close"
+                                        size={24}
+                                        color="#9E9E9E"
+                                        style={{ marginRight: 8 }}
+                                    />
+                                    <Text style={styles.menuItemText}>Close</Text>
+                                </View>
+                            </TouchableOpacity>
+
+                        </View>
+                    )}
+                </TouchableOpacity>
+            </Modal>
+
+            <View style={styles.line} />
+
+        {/*Create new board*/}
+            <TouchableOpacity
+                title="Create New Board"
+                onPress={() => setIsModalVisible(true)}>
+                <MaterialIcons
+                    style={styles.createBoardButton}
+                    name="add"
+                    size={32}
+                    color="#818181"
+                />
+            </TouchableOpacity>
+
+
+        {/* Modal for creating a new board */}
             <Modal
                 visible={isModalVisible}
                 transparent={true}
-                animationType="slide"
+                animationType="none"
                 onRequestClose={() => setIsModalVisible(false)}
             >
                 <View style={styles.modalContainer}>
                     <View style={styles.modalContent}>
-                        <Text style={styles.modalHeader}>Add New Board</Text>
+                        <Text style={styles.createHeader}>
+                            Create New Board
+                        </Text>
                         <TextInput
                             placeholder="Board Name"
+                            placeholderTextColor={'#C4C4C4'}
                             style={styles.input}
                             value={newBoardName}
                             onChangeText={setNewBoardName}
                         />
                         <TextInput
                             placeholder="Thumbnail URL"
+                            placeholderTextColor={'#C4C4C4'}
                             style={styles.input}
                             value={newBoardThumbnail}
                             onChangeText={setNewBoardThumbnail}
                         />
-                        <View style={styles.modalButtons}>
-                            <Button title="Cancel" onPress={() => setIsModalVisible(false)} />
-                            <Button title="Add" onPress={handleAddBoard} />
+                        <View style={styles.createButtons}>
+                            <Button title="Cancel" onPress={() => setIsModalVisible(false)} color='#818181'/>
+                            <Button title="Create" onPress={handleAddBoard} color='#818181' />
                         </View>
                     </View>
                 </View>
@@ -101,72 +216,5 @@ const HomeScreen = ({ navigation }) => {
     );
 };
 
-const styles = StyleSheet.create({
-    scrollContainer: {
-        flexGrow: 1,
-        padding: 16,
-        backgroundColor: '#f8f9fa',
-    },
-    header: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 16,
-    },
-    boardItem: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginBottom: 16,
-        padding: 12,
-        borderWidth: 1,
-        borderColor: '#ddd',
-        borderRadius: 8,
-    },
-    thumbnail: {
-        width: 80,
-        height: 80,
-        borderRadius: 8,
-        marginRight: 12,
-    },
-    boardDetails: {
-        flex: 1,
-    },
-    boardName: {
-        fontSize: 18,
-        fontWeight: '500',
-        marginBottom: 8,
-    },
-    flatListContainer: {
-        paddingBottom: 16,
-    },
-    modalContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    },
-    modalContent: {
-        width: '90%',
-        backgroundColor: 'white',
-        padding: 20,
-        borderRadius: 10,
-    },
-    modalHeader: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginBottom: 16,
-    },
-    input: {
-        height: 40,
-        borderColor: '#ddd',
-        borderWidth: 1,
-        borderRadius: 5,
-        marginBottom: 16,
-        paddingHorizontal: 10,
-    },
-    modalButtons: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-});
 
 export default HomeScreen;
